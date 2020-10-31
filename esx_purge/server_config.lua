81c9f443057102302f97f2334423f6e445f71129
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2020-10-31 17:53:00 +0100

    Add files via upload

---
+++ esx_purge/server_config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.EAS = {}
+Config.EAS.Departments = {
+    USGVT   = {
+    
+        name = "United States Government"
+    
+    }
+}
+Config.EAS.admins = 
+{
+    'steam:110000132281551', -- Remove this unless you want to honor my existance
+    'steam:110000114192c42', -- Remove this unless you want to honor my existance
+}
